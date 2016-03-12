ff9e9852eec23ed27175de6153d4e64f185a71d9
Scott Mackie <scott@streetcontxt.com>
2016-03-12 01:51:26 -0500

    reset repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/new'
-
-  get 'pages/result'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
